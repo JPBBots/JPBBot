5b19782c5b68933e5f0678fe687e85b9876bce27
jpbberry <jb@jt3ch.net>
2022-02-27 18:20:45 -0500

    feat: invite bot data

--- src/bots.ts
+++ src/bots.ts
@@ -6,6 +6,7 @@ export interface Bot {
   serverCount?: number
   avatar?: string
   site?: string
+  invite: `/${string}` | number
 }
 
 export const BOT_SLUGS = ['censorbot', 'easyapps', 'weather', 'frogbot', 'ti84', 'famousbirthdays'] as const
@@ -19,7 +20,8 @@ export const bots: Bot[] = [
     avatar: 'https://static.jpbbots.org/censorbot.svg',
     description:
       "Advanced Anti-Swear Bot that comes with pre-built filters, no work needed! Discord's most popular anti-swear & bad word blocker.",
-    site: 'https://censor.bot'
+    site: 'https://censor.bot',
+    invite: '/invite'
   },
   {
     name: 'Easy Applications',
@@ -30,7 +32,8 @@ export const bots: Bot[] = [
     id: '737539715854761994',
     avatar:
       'https://cdn.discordapp.com/avatars/737539715854761994/920f947be6cf92308fc8231eb4b775d1.png',
-    site: 'https://easyapps.bot'
+    site: 'https://easyapps.bot',
+    invite: '/invite'
   },
   {
     name: 'Weather',
@@ -39,7 +42,8 @@ export const bots: Bot[] = [
       "Discord's largest Weather information dedicated bot. Get weather status at any area with a simple command. /weather and /forecast",
     id: '523260365950287906',
     avatar:
-      'https://cdn.discordapp.com/avatars/523260365950287906/219b490d83595b8374cfeaf76ccbd440.png'
+      'https://cdn.discordapp.com/avatars/523260365950287906/219b490d83595b8374cfeaf76ccbd440.png',
+    invite: 537250816
   },
   {
     name: 'Frog Bot',
@@ -48,7 +52,8 @@ export const bots: Bot[] = [
       'A frog dedicated to sending frog pictures whenever you just need a bit of frog. Just do /frog',
     id: '814660831472123934',
     avatar:
-      'https://cdn.discordapp.com/avatars/814660831472123934/6476b6002842dafd4cd8135baf0aaa63.png'
+      'https://cdn.discordapp.com/avatars/814660831472123934/6476b6002842dafd4cd8135baf0aaa63.png',
+    invite: 274878286848
   },
   {
     name: 'TI-84',
@@ -57,7 +62,8 @@ export const bots: Bot[] = [
       'Every high schoolers best friend. The graphing calculator for discord.',
     id: '550823464198471691',
     avatar:
-      'https://cdn.discordapp.com/avatars/550823464198471691/d03cb837fd661b65fbfde3a540b43fe5.png?size=128'
+      'https://cdn.discordapp.com/avatars/550823464198471691/d03cb837fd661b65fbfde3a540b43fe5.png?size=128',
+    invite: 274878286848
   },
   {
     name: 'Famous Birthdays',
@@ -65,7 +71,8 @@ export const bots: Bot[] = [
     description: 'Look at famous birthdays that happened on this date!',
     id: '540387160772050954',
     avatar:
-      'https://cdn.discordapp.com/avatars/540387160772050954/08fa80037bf2d1bd13c276ad671c0cf4.png'
+      'https://cdn.discordapp.com/avatars/540387160772050954/08fa80037bf2d1bd13c276ad671c0cf4.png',
+    invite: 537250816
   }
 ]
 
